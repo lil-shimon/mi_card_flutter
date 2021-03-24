9d1c346e9182914bdca52322c4b5581bba1e3340
shimosawa <shimon101018@gmail.com>
2021-03-24 22:14:30 +0900

    container widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.pink,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+            child: Text('hello'),
+            color: Colors.white,
+          ),
+        ),
       ),
     );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
